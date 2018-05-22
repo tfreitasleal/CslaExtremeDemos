18144d444398725e64249521a4eddb2ad0fca2c0
tfreitasleal <tfreitasleal@gmail.com>
2018-05-22 11:01:44 +0100

    Update NuGets

--- LazyProperties/ProjectsVendors.WinForms/MainForm.Designer.cs
+++ LazyProperties/ProjectsVendors.WinForms/MainForm.Designer.cs
@@ -82,6 +82,7 @@
             // 
             // MainForm
             // 
+            this.AcceptButton = this.editProject;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(914, 394);
